521c8ee84996682da2e1d248944f72b26ce36a96
MariiaKomarr <104922060+MariiaKomarr@users.noreply.github.com>
2024-12-30 19:13:35 +0200

    Feature8
    Change Feature12 to Feature8 on 18 line))

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Working!! Feature12 is here. Get started by editing <code>src/app/page.tsx</code>.
+            Working!! Feature8 is here. Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>

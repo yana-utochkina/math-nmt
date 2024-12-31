809a7f1a5d44685241fc14cd430811713086cc32
Bohdan Raiskyi <bohdanraiskiy@gmail.com>
2024-12-31 14:06:42 +0200

    commit4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Third commit. Get started by editing <code>src/app/page.tsx</code>.
+            4 commit. Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>

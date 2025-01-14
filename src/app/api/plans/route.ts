84d7891d9eae15352e07592b3457f57795fc2d53
NONAME-lf <big.shef668@gmail.com>
2025-01-14 20:15:47 +0200

    types

--- src/app/api/plans/route.ts
+++ src/app/api/plans/route.ts
@@ -1,5 +1,6 @@
 // Create-запит на створення плану (в параметрах hours : int, endDate : DateType????)
 "use server";
+import { Plan } from "@prisma/client";
 import { prisma } from "../../../lib/db";
 import { NextResponse } from "next/server"
 
@@ -13,84 +14,42 @@ import { NextResponse } from "next/server"
 //     }
 // };
 
-export const GET = async (request: Request) => {
+export const GET = async () => {
     try {
-        const { searchParams } = new URL(request.url);
-        const id = searchParams.get("id");
 
-        if (!id) {
-            try {
-                const plans = await prisma.plan.findMany();
-                return new NextResponse(JSON.stringify(plans), { status: 200 });
-            }
-            catch (error: any) {
-                return new NextResponse("Error in users" + error.message, { status: 500 });
-            }
-        }
-
-        const plans = await prisma.plan.findUnique(
-            {
-                where: {
-                    id: id
-                }
-            }
-        );
-        return new NextResponse(JSON.stringify(plans), { status: 200 });
+        const plans = await prisma.plan.findMany();
+        return NextResponse.json(plans, { status: 200 });
     }
     catch (error: any) {
-        return new NextResponse("Error in users" + error.message, { status: 500 });
+        return NextResponse.json({ error: `Prisma error: ${error.message}` }, { status: 503 });
     }
 };
 
 
 export const POST = async (request: Request) => {
     try {
-        const { hours, endDate } = await request.json();
-        const plan = await prisma.plan.create({
-            data: {
-                hours: hours,
-                endDate: endDate
-            }
-        });
-        return new NextResponse(JSON.stringify({ message: "Plan created", user: plan }), { status: 200 });
+        const { userID, hours, endDate } = await request.json();
+        const newPlan: Plan = {} as Plan;
+        if (!userID) {
+            return NextResponse.json({ error: `Plans error: required user ID and none is provided` }, { status: 503 });
+        }
+        newPlan.userID = userID;
+        if (hours) newPlan.hours = hours;
+        if (endDate) newPlan.endDate = endDate;
+
+
+        const plan = await prisma.plan.create({ data: newPlan });
+        return NextResponse.json(plan, { status: 200 });
     }
     catch (error: any) {
-        return new NextResponse("Error in creating plan" + error.message, { status: 500 });
+        return NextResponse.json({ error: `Prisma error: ${error.message}` }, { status: 503 });
     }
 };
 
 export const PATCH = async (request: Request) => {
-    try {
-        const { id, hours, endDate } = await request.json();
-        const plan = await prisma.plan.update({
-            where: {
-                id: id
-            },
-            data: {
-                hours: hours,
-                endDate: endDate
-            }
-        });
-
-        return new NextResponse(JSON.stringify(plan), { status: 200 });
-    }
-    catch (error: any) {
-        return new NextResponse("Error in plans" + error.message, { status: 500 });
-    }
+    return new NextResponse(`Not implemented error`, { status: 501 });
 }
 
 export const DELETE = async (request: Request) => {
-    try {
-        const { id } = await request.json();
-        const plan = await prisma.plan.delete({
-            where: {
-                id: id
-            }
-        });
-
-        return new NextResponse(JSON.stringify(plan), { status: 200 });
-    }
-    catch (error: any) {
-        return new NextResponse("Error in users" + error.message, { status: 500 });
-    }
+    return new NextResponse(`Not implemented error`, { status: 501 });
 }

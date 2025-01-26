fe7232491cca686f167c210e512fe0e5d21de2ad
Bohdan Raiskyi <bohdanraiskiy@gmail.com>
2025-01-26 13:57:20 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
  const nextConfig: NextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;

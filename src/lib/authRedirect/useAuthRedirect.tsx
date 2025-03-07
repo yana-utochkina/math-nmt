6b9c3b1847be33c1257d3fecfa7fa88214446250
bosvs <eroinevs@gmail.com>
2025-03-08 01:56:30 +0200

    deleted client secret from public commit use auth from roter to navigation

--- src/lib/authRedirect/useAuthRedirect.tsx
+++ src/lib/authRedirect/useAuthRedirect.tsx
@@ -1,5 +1,5 @@
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 const useAuthRedirect = () => {

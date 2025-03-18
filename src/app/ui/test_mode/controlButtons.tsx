179081a775a230c41ebf1e9fb6410d17863002b6
Bohdan Raiskyi <bohdanraiskiy@gmail.com>
2025-03-18 20:33:28 +0200

    added "mobile" view

--- src/app/ui/test_mode/controlButtons.tsx
+++ src/app/ui/test_mode/controlButtons.tsx
@@ -20,30 +20,71 @@ export function ControlButtons({
   nextButtonText
 }: ControlButtonsProps) {
   return (
-    <div className="row justify-content-center mt-3 gap-2">
-      <div className="col-auto">
-        <button className="btn btn-primary custom-button" onClick={onSkip}>
-          Пропустити
-        </button>
-      </div>
-      <div className="col-auto">
+    <div className="d-flex flex-column flex-md-row justify-content-center align-items-center gap-2 mt-3">
+
+      <div className="order-md-2">
         <button 
-          className="btn btn-primary custom-button"
+          className="btn btn-primary custom-button m-1"
           onClick={onSubmit}
           disabled={!canSubmit}
         >
           {nextButtonText || (submitted ? (isLastTask ? "Завершити" : "Далі") : "Відповісти")}
         </button>
       </div>
-      <div className="col-auto">
+      <div className="order-md-3">
         <button
-          className="btn btn-primary custom-button"
+          className="btn btn-primary custom-button m-1"
           onClick={onShowSolution}
           disabled={!submitted}
         >
           {showSolution ? "Сховати розв'язок" : "Розв'язок"}
         </button>
       </div>
+      
+      <div className="order-md-1">
+        <button className="btn btn-primary custom-button m-1" onClick={onSkip}>
+          Пропустити
+        </button>
+      </div>
     </div>
   );
 }
+
+// export function ControlButtons({
+//   onSkip,
+//   onSubmit,
+//   onShowSolution,
+//   submitted,
+//   canSubmit,
+//   showSolution,
+//   isLastTask = false,
+//   nextButtonText
+// }: ControlButtonsProps) {
+//   return (
+//     <div className="row justify-content-center mt-3 gap-2">
+//       <div className="col-auto">
+//         <button className="btn btn-primary custom-button" onClick={onSkip}>
+//           Пропустити
+//         </button>
+//       </div>
+//       <div className="col-auto">
+//         <button 
+//           className="btn btn-primary custom-button"
+//           onClick={onSubmit}
+//           disabled={!canSubmit}
+//         >
+//           {nextButtonText || (submitted ? (isLastTask ? "Завершити" : "Далі") : "Відповісти")}
+//         </button>
+//       </div>
+//       <div className="col-auto">
+//         <button
+//           className="btn btn-primary custom-button"
+//           onClick={onShowSolution}
+//           disabled={!submitted}
+//         >
+//           {showSolution ? "Сховати розв'язок" : "Розв'язок"}
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }

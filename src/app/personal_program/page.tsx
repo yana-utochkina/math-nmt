02f42d1a8d9b4791b5b9ed60007160d766e93c2c
bosvs <eroinevs@gmail.com>
2025-03-19 19:24:11 +0200

    redirect to authorization on personal program

--- src/app/personal_program/page.tsx
+++ src/app/personal_program/page.tsx
@@ -3,10 +3,10 @@
 import { useState } from "react";
 import "../styles/styles.css";
 import { useRouter } from "next/navigation";
-// import useAuthRedirect from "@/lib/authRedirect/useAuthRedirect";
+import useAuthRedirect from "@/lib/authRedirect/useAuthRedirect";
 
 export default function PersonalPlanPage() {
-  // useAuthRedirect();
+  useAuthRedirect();
   const router = useRouter();
   const [hoursNumber, setHoursNumber] = useState("");
   const [endDate, setEndDate] = useState("");

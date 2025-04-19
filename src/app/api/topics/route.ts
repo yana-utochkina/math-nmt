a396767eb7fb4a9e1a484531b0c8fc2dd29201a0
Bohdan Raiskyi <bohdanraiskiy@gmail.com>
2025-04-19 21:09:06 +0300

    topics api fix

--- src/app/api/topics/route.ts
+++ src/app/api/topics/route.ts
@@ -2,22 +2,32 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/lib/db";
 import { Topic } from "@prisma/client";
 
-export async function GET(request: Request) {
+export async function GET() {
   try {
-    // Отримуємо всі теми (можна вибрати лише потрібні поля)
-    const topics = await prisma.topic.findMany({
-      select: {
-        id: true,
-        title: true,
-        // при потребі додайте інші поля
-      },
-    });
-    return NextResponse.json(topics);
-  } catch (error: any) {
-    console.error("Error fetching topics:", error);
-    return NextResponse.json({ error: "Сталася помилка" }, { status: 500 });
+      const topics = await prisma.topic.findMany();
+      return NextResponse.json(topics, { status: 200 });
+  }
+  catch (error) {
+      return NextResponse.json({ error: `Prisma error: ${error.message}` }, { status: 503 });
   }
 }
+
+// export async function GET(request: Request) {
+//   try {
+//     // Отримуємо всі теми (можна вибрати лише потрібні поля)
+//     const topics = await prisma.topic.findMany({
+//       select: {
+//         id: true,
+//         title: true,
+//         // при потребі додайте інші поля
+//       },
+//     });
+//     return NextResponse.json(topics);
+//   } catch (error: any) {
+//     console.error("Error fetching topics:", error);
+//     return NextResponse.json({ error: "Сталася помилка" }, { status: 500 });
+//   }
+// }
 
 export async function POST(request: Request) {
     try {

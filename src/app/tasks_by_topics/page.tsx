e33cce2717c0b0c139c6b34144cc731ba4977301
vseknyaz <eroinevs@gmail.com>
2025-04-20 16:35:54 +0300

    unshelved task by topic authorization check

--- src/app/tasks_by_topics/page.tsx
+++ src/app/tasks_by_topics/page.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from "react";
 import Link from "next/link"; // Використовуємо Link для маршрутизації у Next.js
+import useAuthRedirect from "@/lib/authRedirect/useAuthRedirect";
+
 
 const topics = [
   {
@@ -78,6 +80,7 @@ const topics = [
 ];
 
 export default function TopicsPage() {
+  useAuthRedirect();
   const [expandedTopics, setExpandedTopics] = useState<Record<string, boolean>>({});
 
   const toggleTopic = (title: string) => {
